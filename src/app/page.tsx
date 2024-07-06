5ec79098e5af687f24a2a769c21ac9284a35e194
Eduardo Pereira <egpereira05@gmail.com>
2024-07-06 16:13:23 -0300

    Add UI mock

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
-import Link from "next/link";
+import Link from 'next/link'
+
+const mockURLS = [
+  'https://utfs.io/f/19b821a3-d4a5-4299-8f14-67d3936168de-8fqh8n.jpeg',
+  'https://utfs.io/f/bcf34f98-3d0b-4a57-88f9-e74d955e3aa4-g1tb1c.jpeg',
+  'https://utfs.io/f/5cef7e6e-66fc-4b3c-8aef-5611c5f31463-5frbah.jpeg',
+  'https://utfs.io/f/823532b6-cc06-46df-9f38-6f90bcb58bda-ufkqi2.webp'
+]
+
+const mockImages = mockURLS.map((url, index) => ({
+  id: index + 1,
+  url
+}))
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-     Hello (gallery in progress)
+    <main className=''>
+      <div className='flex flex-wrap gap-4'>
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className='w-48'>
+            <img src={image.url} alt='' />
+          </div>
+        ))}
+      </div>
     </main>
-  );
+  )
 }
